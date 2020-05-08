dc547d1d32701821ddaf7106d9dd26d466cb5fe8
Team <timonsol13@gmail.com>
2020-05-08 18:12:47 +0300

    Init

---
+++ HackgamesVR/Assets/Thirdparty/GameTemplate/Scripts/Debug/DebugHotkeys.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugHotkeys : MonoBehaviour {
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.R)) {
+			SceneManager.LoadScene(0);
+		}
+		else if (Input.GetKeyDown(KeyCode.Escape)) {
+			QuitGame.QuitApp();
+		}
+	}
+}
