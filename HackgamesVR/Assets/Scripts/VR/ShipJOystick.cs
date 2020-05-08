6991b06fa207a72646e36be7edda5d31d7f5fb1f
Team <timonsol13@gmail.com>
2020-05-08 23:57:09 +0300

    Init steamvr

---
+++ HackgamesVR/Assets/Scripts/VR/ShipJOystick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+public class ShipJOystick : MonoBehaviour {
+	public void OnHandHoverBegin(Hand hand) {
+		hand.ShowGrabHint();
+	}
+
+	public void OnHandHoverEnd(Hand hand) {
+		hand.HideGrabHint();
+	}
+}
