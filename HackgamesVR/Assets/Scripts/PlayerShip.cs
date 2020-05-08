a32f72ffdb0a110fb2b62c5f4a7264f3245d2d9f
Team <timonsol13@gmail.com>
2020-05-09 02:40:10 +0300

    Ship forward moving

---
+++ HackgamesVR/Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerShip : MonoBehaviour {
+
+	[SerializeField] ShipJoystick leftJoystick;
+	[SerializeField] ShipJoystick rightJoystick;
+
+	[SerializeField] Rigidbody rb;
+
+#if UNITY_EDITOR
+	private void OnValidate() {
+		if (rb == null)
+			rb = GetComponent<Rigidbody>();
+	}
+#endif
+
+
+	void Update() {
+		if (leftJoystick.transform.localEulerAngles.x != 315f) {
+			rb.velocity = Vector3.forward * 1.0f;
+			Debug.Log("Moving");
+		}
+	}
+}
