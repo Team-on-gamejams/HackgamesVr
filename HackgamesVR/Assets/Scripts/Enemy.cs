35ad17f5542b7bc66746fa3021420d45a2c1118a
Long-as-Python <45416328+Long-as-Python@users.noreply.github.com>
2020-05-09 16:10:57 +0300

    enemy moving

--- HackgamesVR/Assets/Scripts/Enemy.cs
+++ HackgamesVR/Assets/Scripts/Enemy.cs
@@ -4,28 +4,69 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] Rigidbody rb;
+
     public float speed = 100f;
+    public float rotateSpeed = 10f;
     private float waitTime;
     public float startWaitTime;
 
-    public Transform[] moveSpots;
-    private int randomSpot;
+    public Vector3 moveSpot;
+    public Vector3 pos;
+
+    public float maxRadius;
+    public float minRadius;
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (rb == null)
+            rb = GetComponent<Rigidbody>();
+    }
+#endif
 
     private void Start()
     {
         waitTime = startWaitTime;
-        randomSpot = Random.Range(0, moveSpots.Length);
+        spotSearch();
     }
+
+    void spotSearch()
+    {
+        moveSpot = Random.insideUnitSphere * (maxRadius - minRadius);
+
+        if (moveSpot.x < 0)
+            moveSpot.x -= minRadius;
+        else
+            moveSpot.x += minRadius;
+        if (moveSpot.y < 0)
+            moveSpot.y -= minRadius;
+        else
+            moveSpot.y += minRadius;
+        if (moveSpot.z < 0)
+            moveSpot.z -= minRadius;
+        else
+            moveSpot.z += minRadius;
+    }
+
 
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        transform.LookAt(new Vector3(moveSpot.x, moveSpot.x, moveSpot.x));
+    }
 
-        if(Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 1000f)
+    private void FixedUpdate()
+    {
+        Vector3 direction = moveSpot - rb.velocity;
+        Vector3 tmp = Vector3.zero;
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, direction, ref tmp, 0.1f, speed, Time.deltaTime);
+        //rb.angularVelocity = transform.TransformDirection(moveSpot * rotateSpeed);
+        if (Vector3.Distance(transform.position, moveSpot) < 1f)
         {
+            rb.velocity = Vector3.SmoothDamp(rb.velocity, direction, ref tmp, 0.1f, speed, Time.deltaTime);
+            spotSearch();
             if (waitTime <= 0)
             {
-                randomSpot = Random.Range(0, moveSpots.Length);
                 waitTime = startWaitTime;
             }
             else
