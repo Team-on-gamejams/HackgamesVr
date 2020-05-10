968527a808fe5f7341d209946cfdb373c84940b0
Team <timonsol13@gmail.com>
2020-05-10 05:03:06 +0300

    Achievments

---
+++ HackgamesVR/Assets/Scripts/AchievmentUnlocker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievmentUnlocker : MonoBehaviour {
+	[SerializeField] int id = 0;
+
+	bool isUnlocked = false;
+	public void Unlock() {
+		if (isUnlocked)
+			return;
+		isUnlocked = true;
+		GameFlow.instance.UnlockAchievment(id);
+	}
+}
--- HackgamesVR/Assets/Scripts/GameFlow.cs
+++ HackgamesVR/Assets/Scripts/GameFlow.cs
@@ -1,13 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class GameFlow : MonoBehaviour
-{
+public class GameFlow : MonoBehaviour {
+	public static GameFlow instance;
+
+	[Header("Lore")] [Space]
+	[SerializeField] string[] textsMain;
+	[SerializeField] AchievmentText[] textsAchievment;
+
 	[Header("Refs")] [Space]
+	[SerializeField] TextMeshProUGUI mainText;
+	[SerializeField] TextMeshProUGUI achievmentText;
 	[SerializeField] Spawner spawner;
+
+	private void Awake() {
+		instance = this;
+	}
 
 	private void Start() {
 		LeanTween.delayedCall(0.5f, spawner.StartWave);
+	}
+
+	public void UnlockAchievment(int id) {
+		AchievmentText t = textsAchievment[id];
+
+		achievmentText.alpha = 0.0f;
+		LeanTweenEx.ChangeTextAlpha(achievmentText, 1.0f, 0.5f);
+		achievmentText.text = $"{t.textTitle}\n{t.name}\n{t.description}";
+		LeanTweenEx.ChangeTextAlpha(achievmentText, 0.0f, 0.5f)
+			.setDelay(5.0f)
+			.setOnComplete(()=> {
+				achievmentText.text = "  ";
+			});
+	}
+
+	[System.Serializable]
+	struct AchievmentText {
+		public string textTitle;
+		public string name;
+		public string description;
 	}
 }
